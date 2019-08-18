c03d7b1219bf6f673526a4fabe887b2c413f935f
danielbarroslima <daniel.barroslm@gmail.com>
2019-08-18 14:01:51 -0300

    editando receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,18 @@
 class RecipesController < ApplicationController
+  before_action :recipe_find, only: %i[ show edit update]
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
   end
 
   def new
   	@recipe = Recipe.new  	
+  end
+
+  def edit
   end
 
   def create
@@ -20,8 +24,23 @@ class RecipesController < ApplicationController
     end
   end
 
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit  
+      
+    end
+    
+  end
+
   private
     def recipe_params
       params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients,:cook_method)
+    end
+
+    def recipe_find
+      @recipe = Recipe.find(params[:id])
+      
     end
 end
