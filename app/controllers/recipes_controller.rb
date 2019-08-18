357bc9a508b8aee951b66792e06fd1049ca7ed81
danielbarroslima <daniel.barroslm@gmail.com>
2019-08-18 13:27:45 -0300

    cadastrando sem erro

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,22 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+  	@recipe = Recipe.new  	
+  end
+
+  def create
+  	@recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new  
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients,:cook_method)
+    end
 end
